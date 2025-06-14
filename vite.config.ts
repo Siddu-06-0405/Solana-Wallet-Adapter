1d5c7c016c8ecf1792d337dbccdf412d5b7825ee
siddu <csiddhardha0@gmail.com>
2025-06-15 00:20:51 +0530

    reailized base in vite.config.ts is important

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Solana-Wallet-Adapter/", // replace with your repo name
   server: {
     host: "::",
     port: 8080,
